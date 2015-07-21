b24da2d2f0851e159c1457304e7c27ea31daed68
Omar El Gabry <omar_elgabry_93@gmail.com>
2015-07-21 19:34:31 +0200

    Update Encryption.php
    Improve & Secure Encryption methods

--- app/core/Encryption.php
+++ app/core/Encryption.php
@@ -10,6 +10,20 @@
 class Encryption{
 
     /**
+     * Cipher algorithm
+     *
+     * @var string
+     */
+    const CIPHER = 'aes-256-cbc';
+
+    /**
+     * Hash function
+     *
+     * @var string
+     */
+    const HASH_FUNCTION = 'sha256';
+
+    /**
      * constructor for Encryption object.
      *
      * @access private
@@ -18,36 +32,56 @@ class Encryption{
 
     /**
      * Encrypt an id.
-     * This method encrypt by converting to base 16, after some random multiply and additions.
      *
      * @access public
      * @static static method
      * @param  integer|string	$id
      * @return string
+     * @see http://kvz.io/blog/2009/06/10/create-short-ids-with-php-like-youtube-or-tinyurl/
      */
     public static function encryptId($id){
 
-        $enctypId = base_convert(($id * 9518436) + 1142, 10, 16);
-        return $enctypId;
+        $encryptId  = "";
+        $chars = self::getCharacters();
+        $base  = strlen($chars);
+        $id    = ((int)$id * 9518436) + 1142;
+
+        for ($t = ($id != 0 ? floor(log($id, $base)) : 0); $t >= 0; $t--) {
+            $bcp = bcpow($base, $t);
+            $a   = floor($id / $bcp) % $base;
+            $encryptId = $encryptId . substr($chars, $a, 1);
+            $id  = $id - ($a * $bcp);
+        }
+
+        return $encryptId;
     }
 
     /**
      * Decryption for Id.
-     * This method decrypt by converting to base 10, then division and subtraction.
      *
      * @access public
      * @static static method
      * @param  string	$id
      * @return integer
+     * @throws Exception if $id is empty
      */
     public static function decryptId($id){
 
-        if(empty($id)){ return 0; }
+        if(empty($id)){
+            throw new Exception("the id to decrypt can't be empty");
+        }
 
-        $decrypId = (int)base_convert($id, 16, 10);
-        $decrypId = ($decrypId - 1142) / 9518436;
+        $decryptId  = "";
+        $chars = self::getCharacters();
+        $base  = strlen($chars);
+        $len   = strlen($id) - 1;
 
-        return (int)$decrypId;
+        for ($t = $len; $t >= 0; $t--) {
+            $bcp = bcpow($base, $len - $t);
+            $decryptId = $decryptId + strpos($chars, substr($id, $t, 1)) * $bcp;
+        }
+
+        return ((int)$decryptId - 1142) / 9518436;
     }
 
     /**
@@ -57,16 +91,56 @@ class Encryption{
      * @static static method
      * @param  string	$id
      * @return integer
+     * @throws Exception if $id is empty
      */
     public static function decryptIdWithDash($id){
 
-        if(empty($id)){ return 0; }
+        if(empty($id)){
+            throw new Exception("the id to decrypt can't be empty");
+        }
 
-        $decrypId = explode("-", $id)[1];
-        $decrypId = (int)base_convert($decrypId, 16, 10);
-        $decrypId = ($decrypId - 1142) / 9518436;
+        $decryptId  = "";
+        $chars = self::getCharacters();
+        $base  = strlen($chars);
+        $id    = explode("-", $id)[1];
 
-        return (int)$decrypId ;
+        $len = strlen($id) - 1;
+
+        for ($t = $len; $t >= 0; $t--) {
+            $bcp = bcpow($base, $len - $t);
+            $decryptId = $decryptId + strpos($chars, substr($id, $t, 1)) * $bcp;
+        }
+
+        return ((int)$decryptId - 1142) / 9518436;
+    }
+
+    /**
+     * get characters that will be used in encryption/decryption provided by a key
+     *
+     * @access private
+     * @static static method
+     * @return string
+     * @throws Exception if $id is empty
+     */
+    private static function getCharacters(){
+
+        $chars  = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        for ($n = 0; $n < strlen($chars); $n++) {
+            $i[] = substr($chars, $n, 1);
+        }
+
+        $key_hash = hash('sha256', HASH_KEY);
+        $key_hash = (strlen($key_hash) < strlen($chars) ? hash('sha512', HASH_KEY) : $key_hash);
+
+        for ($n = 0; $n < strlen($chars); $n++) {
+            $p[] =  substr($key_hash, $n, 1);
+        }
+
+        array_multisort($p, SORT_DESC, $i);
+        $chars = implode($i);
+
+        return $chars;
     }
 
     /**
@@ -74,26 +148,35 @@ class Encryption{
      *
      * @access public
      * @static static method
-     * @param  string	$str
+     * @param  string	$plain
      * @return string
+     * @throws Exception If functions don't exists
      */
-    public static function encrypt($str){
+    public static function encrypt($plain){
 
-        //choose cipher type and mode, must match the decrypt() method
-        $cipher_type = MCRYPT_RIJNDAEL_256;
-        $cipher_mode = MCRYPT_MODE_CBC;
+        if(!function_exists('openssl_cipher_iv_length') ||
+            !function_exists('openssl_random_pseudo_bytes') ||
+            !function_exists('openssl_encrypt')){
+            throw new Exception("Encryption function don't exists");
+        }
 
-        //initialization vector adds more security
-        //this will make the $iv different every time
-        //Thus, the encrypted string will be different for the same original string.
-        $iv_size = mcrypt_get_iv_size($cipher_type, $cipher_mode);
-        $iv      = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+        //generate initialization vector,
+        //this will make $iv different every time,
+        //so, encrypted string will be also different.
+        $iv_size = openssl_cipher_iv_length(self::CIPHER);
+        $iv      = openssl_random_pseudo_bytes($iv_size);
 
-        $encrypted_string = mcrypt_encrypt($cipher_type, ENCRYPTION_KEY, $str, $cipher_mode, $iv);
+        //generate key for authentication using ENCRYPTION_KEY & HMAC_SALT
+        $key = mb_substr(hash(self::HASH_FUNCTION, ENCRYPTION_KEY . HMAC_SALT), 0, 32, '8bit');
 
-        //return initialization vector + encrypted string
-        //because it's needed when decrypting.
-        return base64_encode($iv . $encrypted_string);
+        //append initialization vector
+        $encrypted_string = openssl_encrypt($plain, self::CIPHER, $key, OPENSSL_RAW_DATA, $iv);
+        $ciphertext       = $iv . $encrypted_string;
+
+        //apply the HMAC
+        $hmac = hash_hmac('sha256', $ciphertext, $key);
+
+        return $hmac . $ciphertext;
     }
 
     /**
@@ -101,24 +184,74 @@ class Encryption{
      *
      * @access public
      * @static static method
-     * @param  string	$str
+     * @param  string $ciphertext
      * @return string
+     * @throws Exception If $ciphertext is empty, or If functions don't exists
      */
-    public static function decrypt($str){
+    public static function decrypt($ciphertext){
 
-        $str = base64_decode($str);
+        if(empty($ciphertext)){
+            throw new Exception("the string to decrypt can't be empty");
+        }
 
-        //choose cipher type and mode, must match the encrypt() method
-        $cipher_type = MCRYPT_RIJNDAEL_256;
-        $cipher_mode = MCRYPT_MODE_CBC;
+        if(!function_exists('openssl_cipher_iv_length') ||
+            !function_exists('openssl_decrypt')){
+            throw new Exception("Encryption function don't exists");
+        }
 
-        //get the initialization vector from the encrypted string.
-        //The $iv has fixed size according to the cipher and mode used.
-        $iv_size = mcrypt_get_iv_size($cipher_type, $cipher_mode);
-        $iv      = substr($str, 0, $iv_size);
-        $encrypted_string = substr($str, $iv_size);
+        //generate key used for authentication using ENCRYPTION_KEY & HMAC_SALT
+        $key = mb_substr(hash(self::HASH_FUNCTION, ENCRYPTION_KEY . HMAC_SALT), 0, 32, '8bit');
 
-        $string = rtrim(mcrypt_decrypt($cipher_type, ENCRYPTION_KEY, $encrypted_string, $cipher_mode, $iv), "\0");
-        return $string;
+        //split cipher into: hmac, cipher & iv
+        $macSize    = 64;
+        $hmac 	    = mb_substr($ciphertext, 0, $macSize, '8bit');
+        $iv_cipher  = mb_substr($ciphertext, $macSize, null, '8bit');
+
+        //generate original hmac & compare it with the one in $ciphertext
+        $originalHmac = hash_hmac('sha256', $iv_cipher, $key);
+        if(!self::hashEquals($hmac, $originalHmac)){
+            return false;
+        }
+
+        //split out the initialization vector and cipher
+        $iv_size = openssl_cipher_iv_length(self::CIPHER);
+        $iv      = mb_substr($iv_cipher, 0, $iv_size, '8bit');
+        $cipher  = mb_substr($iv_cipher, $iv_size, null, '8bit');
+
+        return openssl_decrypt($cipher, self::CIPHER, $key, OPENSSL_RAW_DATA, $iv);
     }
-} 
+
+    /**
+     * A timing attack resistant comparison.
+     *
+     * @access private
+     * @static static method
+     * @param string $hmac The hmac from the ciphertext being decrypted.
+     * @param string $compare The comparison hmac.
+     * @return bool
+     * @see https://github.com/sarciszewski/php-future/blob/bd6c91fb924b2b35a3e4f4074a642868bd051baf/src/Security.php#L36
+     */
+    private static function hashEquals($hmac, $compare){
+
+        if (function_exists('hash_equals')) {
+            return hash_equals($hmac, $compare);
+        }
+
+        //if hash_equals() is not available,
+        //then use the following snippet.
+        //It's equivalent to hash_equals() in PHP 5.6.
+        $hashLength    = mb_strlen($hmac, '8bit');
+        $compareLength = mb_strlen($compare, '8bit');
+
+        if ($hashLength !== $compareLength) {
+            return false;
+        }
+
+        $result = 0;
+        for ($i = 0; $i < $hashLength; $i++) {
+            $result |= (ord($hmac[$i]) ^ ord($compare[$i]));
+        }
+
+        return $result === 0;
+    }
+}

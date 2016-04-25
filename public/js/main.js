19f587f279a1c7e60ba43f04f956cd333add4251
Omar El Gabry <omar.elgabry.93@gmail.com>
2016-04-25 17:04:24 +0200

    #8 Fix Login tabs events
    Tabs at login page were removed by mistake

--- public/js/main.js
+++ public/js/main.js
@@ -495,6 +495,30 @@ var events = {
      */
     login: {
         init: function(){
+            events.login.tabs();
+        },
+        tabs: function(){
+            $("#form-login #link-forgot-password, #form-forgot-password #link-login").click(function() {
+                
+                $("#form-login, #form-forgot-password" ).toggleClass("display-none");
+                $(".error, .success").remove();
+            });
+
+            $("#link-register").click(function() {
+                
+                $("#form-login, #form-forgot-password").addClass("display-none");
+                $("#form-register").removeClass("display-none");
+                $(".panel-title").text("Register");
+                $(".error, .success").remove();
+            });
+
+            $("#form-register #link-login").click(function() {
+                
+                $(".panel-title").text("Login");
+                $("#form-register").addClass("display-none");
+                $("#form-login").removeClass("display-none");
+                $(".error, .success").remove();
+            });
         }
     },
 
